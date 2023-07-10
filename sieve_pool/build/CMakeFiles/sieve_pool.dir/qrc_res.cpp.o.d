b76f48ee84d0e985af64840029ba2a57a577fbc3
crucal <s18908155520@163.com>
2023-07-10 17:31:21 +0800

    add Cmake_build_qrcc_files

---
+++ sieve_pool/build/CMakeFiles/sieve_pool.dir/qrc_res.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/sieve_pool.dir/qrc_res.cpp.o: \
+ /home/crucal/workspace/myqt/qt6/sieve_pool/build/qrc_res.cpp \
+ /usr/include/stdc-predef.h
---
+++ sieve_pool/build/CMakeFiles/sieve_pool_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sieve_pool_autogen"
+  "sieve_pool_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/sieve_pool_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
