9827bd1d447292c5408454923c9aa03d01b3e8df
crucal <S18908155520@163.com>
2023-08-25 11:11:56 +0800

    snake-game

---
+++ snake-game/snake.h
@@ -0,0 +1,43 @@
+#ifndef SNAKE_H
+#define SNAKE_H
+
+#include <QGraphicsItem>
+#include <QRectF>
+
+class GameController;
+
+class Snake : public QGraphicsItem {
+public:
+  enum Direction { NoMove, MoveLeft, MoveRight, MoveUp, MoveDown };
+
+  Snake(GameController &controller);
+
+  QRectF boundingRect() const;
+  QPainterPath shape() const;
+  void paint(QPainter *painter, const QStyleOptionGraphicsItem *, QWidget *);
+
+  void setMoveDirection(Direction direction);
+  Direction currentDirection();
+
+protected:
+  void advance(int step);
+
+private:
+  template <typename T> void move(T &pos, int offset, int max, int min);
+  void moveLeft();
+  void moveRight();
+  void moveUp();
+  void moveDown();
+
+  void handleCollisions();
+
+  QPointF head;
+  int growing;
+  int speed;
+  QList<QPointF> tail;
+  int tickCounter;
+  Direction moveDirection;
+  GameController &controller;
+};
+
+#endif // SNAKE_H
