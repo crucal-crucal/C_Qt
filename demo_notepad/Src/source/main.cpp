a3e05898aedba210aab3ffe3967a4cab355fdc74
crucal <s18908155520@163.com>
2023-11-15 12:33:58 +0800

    demo_notepad --version 2.0

--- demo_notepad/Src/source/main.cpp
+++ demo_notepad/Src/source/main.cpp
@@ -1,11 +1,10 @@
-#include "Src/include/mainwindow.h"
-
 #include <QApplication>
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+#include "Src/include/mainwindow.h"
+
+int main(int argc, char *argv[]) {
+  QApplication a(argc, argv);
+  MainWindow w;
+  w.show();
+  return a.exec();
 }
