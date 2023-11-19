7e0878532e6efbd8bc68b96d6827f63458440627
crucal <s18908155520@163.com>
2023-11-19 18:10:12 +0800

    update

--- TCPtest/main.cpp
+++ TCPtest/main.cpp
@@ -1,8 +1,11 @@
-#include <QApplication>
 
 #include "mainwindow.h"
 
-int main(int argc, char* argv[]) {
+#include <QApplication>
+
+
+int main(int argc, char *argv[])
+{
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
